84f07eb640356f2822474c36773374cba3e36542
0shiros <oli2014@gmx.fr>
2025-11-10 16:28:20 +0100

    Initial commit

---
+++ Source/SnoWar/SnoWar.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnoWar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnoWar, "SnoWar" );
